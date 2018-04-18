a1428c745c2597410db6b0b35117e23615480909
hezhuangzhuang <1034097946@qq.com>
2018-04-18 14:19:22 +0800

    update

--- hz-web/src/test/java/com/test/dap/TestSpringDao.java
+++ hz-web/src/test/java/com/test/dap/TestSpringDao.java
@@ -6,14 +6,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpringDao {
-
-
-
-
-
-
     private static  ApplicationContext applicationContext;
-
     public static  void  main(String args[]){
         applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext-dao.xml");
         Object obj = applicationContext.getBean("sqlSessionFactory");
