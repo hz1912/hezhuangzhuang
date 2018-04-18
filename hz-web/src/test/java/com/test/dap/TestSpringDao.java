8fccd5a22b4c946c5cd27fb8140fbb3ed63d91dc
hezhuangzhuang <1034097946@qq.com>
2018-04-18 23:54:15 +0800

    update

--- hz-web/src/test/java/com/test/dap/TestSpringDao.java
+++ hz-web/src/test/java/com/test/dap/TestSpringDao.java
@@ -1,17 +1,21 @@
 package com.test.dap;
 
+import com.hz.hz.service.adminservice.AdminService;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.hz.hz.persistence.model.Admin;
 
 public class TestSpringDao {
-    private static  ApplicationContext applicationContext;
-    public static  void  main(String args[]){
-        applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext-dao.xml");
-        Object obj = applicationContext.getBean("sqlSessionFactory");
-        System.out.println(obj);
+    private static ApplicationContext applicationContext;
+
+    public static void main(String args[]) {
+        applicationContext = new ClassPathXmlApplicationContext("classpath:applicationcontext-dao.xml");
+        AdminService adminService = (AdminService) applicationContext.getBean("adminServiceImpl");
+        System.out.println(adminService);
+        Admin admin = adminService.getAdminById(1);
+        System.out.println(admin.getName());
+
     }
-
-
 }
