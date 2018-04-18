34158adc38d8352b11c6813fd80e54afe9dc045a
hezhuangzhuang <1034097946@qq.com>
2018-04-18 13:35:01 +0800

    first commit

---
+++ hz-persistence/src/main/java/com/hz/persistence/Interceptor/SqlInterceptor.java
@@ -0,0 +1,20 @@
+package com.hz.persistence.Interceptor;
+
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Invocation;
+
+import java.util.Properties;
+
+public class SqlInterceptor implements Interceptor {
+    public Object intercept(Invocation invocation) throws Throwable {
+        return null;
+    }
+
+    public Object plugin(Object target) {
+        return null;
+    }
+
+    public void setProperties(Properties properties) {
+
+    }
+}
---
+++ hz-web/src/test/java/com/test/dap/TestSpringDao.java
@@ -0,0 +1,20 @@
+package com.test.dap;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpringDao {
+
+
+    private static  ApplicationContext applicationContext;
+
+    public static  void  main(String args[]){
+        applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext-dao.xml");
+        Object obj = applicationContext.getBean("sqlSessionFactory");
+        System.out.println(obj);
+    }
+
+
+}
